3930889e35ef6c81fe786cb3f481744f4b2e7623
abarnamurugesan <abarnamurugesan17@gmail.com>
2022-02-06 17:24:24 +0530

    New project

---
+++ src/org/cd/Company.java
@@ -0,0 +1,11 @@
+package org.cd;
+
+public class Company {
+	public void compId() {
+		System.out.println("4546");
+	}
+public static void main(String[] args) {
+	Company c = new Company();
+	c.compId();
+}
+}
